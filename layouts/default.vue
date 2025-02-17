e1a085737ae4cdc69b96f25f6e8e43625b2b5732
rodri542 <l20120987@morelia.tecnm.mx>
2025-02-16 22:40:15 -0600

    responsive side bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,21 @@
-<script setup lang="ts"></script>
 <template>
   <div class="layout">
     <AppHeader />
     <main class="main__index">
-      <LeftSideBar />
+      <LeftSideBar class="sidebar" />
       <slot />
     </main>
     <AppFooter />
   </div>
 </template>
+
+<style lang="scss" scoped>
+@use '/assets/styles/main.scss' as *;
+
+.sidebar {
+  @include responsive('sm') {
+    grid-template-columns: repeat(3, 1fr);
+    display: none;
+  }
+}
+</style>
