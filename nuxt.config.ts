e15affb6147941d37040d92e834b5261e48fb4b0
rodri542 <l20120987@morelia.tecnm.mx>
2025-02-18 18:01:11 -0600

    cloud flare conf

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-// import { cssImports } from './config/css'
 
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  nitro: {
+    preset: 'cloudflare',
+    serveStatic: true,
+  },
   modules: ['@nuxt/eslint', '@pinia/nuxt'],
   css: ['~/assets/styles/reset.css', '~/assets/styles/main.scss'],
-  // css: cssImports,
-  // vite: {
-  //   css: {
-  //     preprocessorOptions: {
-  //       scss: {
-  //         additionalData: '@use "sass:map"; @import "assets/styles/main.scss";',
-  //       },
-  //     },
-  //   },
-  // },
   runtimeConfig: {
     twitchClientSecret: process.env.TWITCH_CLIENT_SECRET,
 
