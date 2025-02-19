6665c6bc7a17792ec463cfe49b6c024c1e8f3ea4
rodri542 <l20120987@morelia.tecnm.mx>
2025-02-18 18:57:32 -0600

    netlify proyect

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,7 @@
 
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  nitro: {
-    preset: 'cloudflare_pages',
-  },
+  devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@pinia/nuxt'],
   css: ['~/assets/styles/reset.css', '~/assets/styles/main.scss'],
   runtimeConfig: {
